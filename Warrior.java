f2457c4d5bf33ca49abbc9b21925a05fa092551a
Calvin Ye <you@example.com>
2024-12-13 12:03:26 -0500

    Added Warrior and Weapon

---
+++ Warrior.java
@@ -0,0 +1,15 @@
+class Warrior extends Adventurer {
+    private String weapon;
+
+    public Warrior(String name, String weapon){
+        super(name, 10);
+        this.weapon = weapon;
+    }
+
+    public Warrior(String name, int hp, String weapon){
+        super(name, hp);
+        this.weapon = weapon;
+    }
+
+    public attack
+}
